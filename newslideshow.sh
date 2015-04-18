18d3da15a0ac1cc556a36780f0332ebc887c6c0a
Michael F. Schönitzer <michael@schoenitzer.de>
2015-04-19 00:40:25 +0200

    added slideshow-script

---
+++ newslideshow.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+timetosleep=$1
+cd $2
+
+resolution=$(fbset -s -fb /dev/fb0 | grep ^mode | cut -d\" -f2)
+
+first=1
+for i in *jpg; do
+   # Note: Convert has different resizing techniques
+   # best:        -rescale
+   # in between:  -scale
+   # fast:        -sample
+   convert -sample $resolution -gravity center -background black -extent $resolution $i - | \
+   stream -map bgra -extract $resolution -virtual-pixel black -storage-type char jpg:- /dev/shm/pic.dat &
+   
+   if [[ $first -eq 1 ]] ; then
+     first=0
+   else
+     sleep $timetosleep
+   fi
+   
+   while jobs -rp | grep -q ''; do
+     sleep 0.1
+   done
+   
+   cp /dev/shm/pic.dat /dev/fb0
+done
+
+
