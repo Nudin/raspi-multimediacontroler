465175ccc92d23e58f94c306b19c4b8711cb472a
Michael F. Schönitzer <ms@pi3g.com>
2015-08-13 16:01:06 +0200

    Revert "k"
    This reverts commit ce624db315bb21ed8a279587e4ae33c593dce64d.

--- settings.py
+++ settings.py
@@ -1,1 +1,25 @@
-/boot/MEDIA/settings.py
+
+# Player-software
+videoplayer = "omxplayer -b"
+#videoplayer = "mpv"
+imageplayer = "./newslideshow.sh 4"
+#imageplayer = "eom"
+
+# Contentfolder
+videodir="/boot/MEDIA/Videos/"
+photodir="/boot/MEDIA/Photos/"
+
+# Design
+gtktheme="Clearlooks"
+font="sans normal 9"
+touch_resolution_w=320
+touch_resolution_h=240
+show_exit=True
+
+# Defaults
+play_on_start_type='image'	# Play something on start? Values: None, 'video', 'image'
+play_on_start_file='/boot/MEDIA/Photos/Youtube'	# Play this file 
+autoplay_default=True
+
+# Other
+DEBUG=True
